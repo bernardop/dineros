269201cb92a617a9355ad79a85a797dbf8fc4b46
Bernardo Pacheco <bernardo.pacheco@dominos.com>
2016-07-30 10:23:00 -0400

    Added aphrodite for styling, I dig it

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import { StyleSheet, css } from 'aphrodite';
 
 const App = () => {
   return (
-    <AppBar title='Dineros' />
+    <div>
+      <AppBar title='Dineros' showMenuIconButton={false} />
+      <div className={css(styles.textColor)}>
+        Los dineros que debemos
+      </div>
+    </div>
   );
 };
+
+const styles = StyleSheet.create({
+    textColor: {
+        color: '#fff',
+        padding: '25px'
+    }
+});
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 import App from './components/App';
+import './styles/index.css';
 
 injectTapEventPlugin();
 
 render(
-  <MuiThemeProvider>
+  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
     <App />
   </MuiThemeProvider>,
   document.getElementById('app')
